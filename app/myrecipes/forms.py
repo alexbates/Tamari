798e55ca00f73df6966c5152d848aa13ea1350b3
Alex Bates <alex.bates@me.com>
2024-04-08 16:27:06 -0400

    Add form validation for integer fields

--- app/myrecipes/forms.py
+++ app/myrecipes/forms.py
@@ -4,10 +4,19 @@ from wtforms import StringField, IntegerField, PasswordField, BooleanField, Text
 from wtforms.validators import ValidationError, DataRequired, Optional, Email, EqualTo, Length
 from app.models import User
 
+# Validation for string and textarea fields
+# Prevents input of code that a browser may interpret as HTML, CSS, or Javascript
 def disallowed_chars(form, field):
     dis_chars = {'<', '>', '{', '}', '/*', '*/', ';'}
     if any(char in dis_chars for char in field.data):
         raise ValidationError('Cannot contain <, >, {, }, ;, or *')
+
+# Validation for integer fields
+def validate_integer(form, field):
+    try:
+        int(field.data)
+    except ValueError:
+        raise ValidationError('Must be integer')
 
 class DisplaySettingsForm(FlaskForm):
     recipe_size = RadioField('Recipe Size', choices=[(0, 'Large'),(1, 'Small')], default=lambda: current_user.pref_size)
@@ -23,18 +32,18 @@ class AddRecipeForm(FlaskForm):
     category = SelectField(validators=[DataRequired()], choices=[])
     description = TextAreaField(validators=[Length(0,500),disallowed_chars])
     url = StringField(validators=[Length(0,200),disallowed_chars])
-    servings = IntegerField(validators=[Optional()])
-    prep_time = IntegerField(validators=[Optional()])
-    cook_time = IntegerField(validators=[Optional()])
-    total_time = IntegerField(validators=[Optional()])
-    n_calories = IntegerField(validators=[Optional()])
-    n_carbs = IntegerField(validators=[Optional()])
-    n_protein = IntegerField(validators=[Optional()])
-    n_fat = IntegerField(validators=[Optional()])
-    n_sugar = IntegerField(validators=[Optional()])
-    n_cholesterol = IntegerField(validators=[Optional()])
-    n_sodium = IntegerField(validators=[Optional()])
-    n_fiber = IntegerField(validators=[Optional()])
+    servings = IntegerField(validators=[Optional(),validate_integer])
+    prep_time = IntegerField(validators=[Optional(),validate_integer])
+    cook_time = IntegerField(validators=[Optional(),validate_integer])
+    total_time = IntegerField(validators=[Optional(),validate_integer])
+    n_calories = IntegerField(validators=[Optional(),validate_integer])
+    n_carbs = IntegerField(validators=[Optional(),validate_integer])
+    n_protein = IntegerField(validators=[Optional(),validate_integer])
+    n_fat = IntegerField(validators=[Optional(),validate_integer])
+    n_sugar = IntegerField(validators=[Optional(),validate_integer])
+    n_cholesterol = IntegerField(validators=[Optional(),validate_integer])
+    n_sodium = IntegerField(validators=[Optional(),validate_integer])
+    n_fiber = IntegerField(validators=[Optional(),validate_integer])
     ingredients = TextAreaField(validators=[DataRequired(),Length(1,2200),disallowed_chars])
     instructions = TextAreaField(validators=[DataRequired(),Length(1,6600),disallowed_chars])
     submit = SubmitField('Submit')
@@ -44,18 +53,18 @@ class EditRecipeForm(FlaskForm):
     category = SelectField(validators=[DataRequired()], coerce=str, choices=[])
     description = TextAreaField(validators=[Length(0,500),disallowed_chars])
     url = StringField(validators=[Length(0,200),disallowed_chars])
-    servings = IntegerField(validators=[Optional()])
-    prep_time = IntegerField(validators=[Optional()])
-    cook_time = IntegerField(validators=[Optional()])
-    total_time = IntegerField(validators=[Optional()])
-    n_calories = IntegerField(validators=[Optional()])
-    n_carbs = IntegerField(validators=[Optional()])
-    n_protein = IntegerField(validators=[Optional()])
-    n_fat = IntegerField(validators=[Optional()])
-    n_sugar = IntegerField(validators=[Optional()])
-    n_cholesterol = IntegerField(validators=[Optional()])
-    n_sodium = IntegerField(validators=[Optional()])
-    n_fiber = IntegerField(validators=[Optional()])
+    servings = IntegerField(validators=[Optional(),validate_integer])
+    prep_time = IntegerField(validators=[Optional(),validate_integer])
+    cook_time = IntegerField(validators=[Optional(),validate_integer])
+    total_time = IntegerField(validators=[Optional(),validate_integer])
+    n_calories = IntegerField(validators=[Optional(),validate_integer])
+    n_carbs = IntegerField(validators=[Optional(),validate_integer])
+    n_protein = IntegerField(validators=[Optional(),validate_integer])
+    n_fat = IntegerField(validators=[Optional(),validate_integer])
+    n_sugar = IntegerField(validators=[Optional(),validate_integer])
+    n_cholesterol = IntegerField(validators=[Optional(),validate_integer])
+    n_sodium = IntegerField(validators=[Optional(),validate_integer])
+    n_fiber = IntegerField(validators=[Optional(),validate_integer])
     ingredients = TextAreaField(validators=[DataRequired(),Length(1,2200),disallowed_chars])
     instructions = TextAreaField(validators=[DataRequired(),Length(1,6600),disallowed_chars])
     submit = SubmitField('Save')

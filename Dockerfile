974771e677e7b6aa7f6da330057134d025b67414
Alex Bates <alex.bates@me.com>
2024-05-27 14:43:51 -0400

    Fix for Docker volume data copy issue, Tamari will now populate recipe-photos directory every time container boots

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 
 COPY app app
+COPY rpdocker rpdocker
 COPY tamari.py config.py boot.sh ./
 RUN chmod a+x boot.sh
 
--- boot.sh
+++ boot.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+
+# Initialize and upgrade the database 
 flask db init
 flask db migrate
 flask db upgrade
+
+# Create recipe-photos directory in volume if it doesn't exist, will not result in error
+mkdir /app/appdata/recipe-photos || true
+
+# Populate recipe-photos directory, will not result in error
+cp /rpdocker/default{01..27}.png /app/appdata/recipe-photos/ || true
+
+# Start the application
 exec gunicorn -b 0.0.0.0:4888 -w 4 app:app

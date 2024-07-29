3a0aa03b8ef9ecd3b786e787796a420fbe5e0ce8
Alex Bates <alex.bates@me.com>
2024-07-29 14:31:04 -0400

    Prevent deletion of Demo photos in Remove Recipe and Edit Recipe routes

--- app/myrecipes/routes.py
+++ app/myrecipes/routes.py
@@ -937,7 +937,8 @@ def removeRecipe(hexid):
     defaults = ['default01.png', 'default02.png', 'default03.png', 'default04.png', 'default05.png', 'default06.png', 'default07.png',
         'default08.png', 'default09.png', 'default10.png', 'default11.png', 'default12.png', 'default13.png', 'default14.png',
         'default15.png', 'default16.png', 'default17.png', 'default18.png', 'default19.png', 'default20.png', 'default21.png',
-        'default22.png', 'default23.png', 'default24.png', 'default25.png', 'default26.png', 'default27.png']
+        'default22.png', 'default23.png', 'default24.png', 'default25.png', 'default26.png', 'default27.png' 'demo1.jpg', 'demo2.jpg',
+        'demo3.jpg', 'demo4.jpg', 'demo5.jpg', 'demo6.jpg', 'demo7.jpg', 'demo8.jpg', 'demo9.jpg', 'demo10.jpg', 'demo11.jpg', 'demo12.jpg']
     fullpath = app.config['UPLOAD_FOLDER'] + '/' + delrecipe.photo
     if delrecipe.photo not in defaults:
         try:
@@ -1644,7 +1645,9 @@ def editRecipe(hexid):
             defaults = ['default01.png', 'default02.png', 'default03.png', 'default04.png', 'default05.png', 'default06.png', 'default07.png',
                 'default08.png', 'default09.png', 'default10.png', 'default11.png', 'default12.png', 'default13.png', 'default14.png',
                 'default15.png', 'default16.png', 'default17.png', 'default18.png', 'default19.png', 'default20.png', 'default21.png',
-                'default22.png', 'default23.png', 'default24.png', 'default25.png', 'default26.png', 'default27.png']
+                'default22.png', 'default23.png', 'default24.png', 'default25.png', 'default26.png', 'default27.png' 'demo1.jpg', 'demo2.jpg',
+                'demo3.jpg', 'demo4.jpg', 'demo5.jpg', 'demo6.jpg', 'demo7.jpg', 'demo8.jpg', 'demo9.jpg', 'demo10.jpg', 'demo11.jpg',
+                'demo12.jpg']
             filename, file_extension = os.path.splitext(image.filename)
             hex_valid = 0
             while hex_valid == 0:

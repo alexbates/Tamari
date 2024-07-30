8097d59f703744119484d9f924a4762e9e41d247
Alex Bates <alex.bates@me.com>
2024-07-30 14:43:53 -0400

    Update gunicorn command in start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source venv/bin/activate
-gunicorn -b 0.0.0.0:4888 -w 4 app:app
+gunicorn -b 0.0.0.0:4888 -w 4 --error-logfile error.log --access-logfile >(python app/log_filter.py > access.log) app:app

dd2c175f427c9abf9887ad7f87c23ab77ee3e6d8
Alex Bates <alex.bates@me.com>
2024-09-23 21:56:43 -0400

    Add support in config.py for Chinese (Simplified) and add new messages.po to translations directory

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app/appdata/app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    LANGUAGES = ['en', 'es', 'de']
+    LANGUAGES = ['en', 'es', 'de', 'zh_Hans']
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None

5621f6c220335237a02e0032467e9a3975c09145
Alex Bates <alex.bates@me.com>
2024-10-21 13:08:09 -0400

    Add 1 second delay when processing API login requests

--- app/api/routes.py
+++ app/api/routes.py
@@ -17,6 +17,8 @@ def apiAuthenticate():
     if app.config.get('API_ENABLED', True):
         # Call rate limited function to effectively impose rate limit on login attempts
         if rate_limited_login():
+            # Add delay to slow the rate at which login requests can be made
+            time.sleep(1)
             data = request.get_json()
             app_name = request.headers.get('X-App-Name')
             app_key = request.headers.get('X-App-Key')

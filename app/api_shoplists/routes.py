a632269cb4abfe634299c4c03734e93fb2dc3bca
Alex Bates <alex.bates@me.com>
2024-11-10 14:53:53 -0500

    Add API Remove Shopping List route

--- app/api_shoplists/routes.py
+++ app/api_shoplists/routes.py
@@ -214,6 +214,46 @@ def apiAddListItem(hexid):
     else:
         return jsonify({"message": "API is disabled"}), 503
         
+@bp.route('/api/shopping-lists/<hexid>/remove', methods=['DELETE'])
+@limiter.limit(Config.DEFAULT_RATE_LIMIT)
+@jwt_required()
+# If provided token in Authorization header is an access_token, it will fail with 401 Unauthorized
+# This route removes a shopping list by hexid, along with all associated list items
+def apiRemoveShoppingList(hexid):
+    if app.config.get('API_ENABLED', True):
+        # Check if there is a request body (there should be none)
+        if request.data:
+            return jsonify({"message": "Request body is not allowed"}), 400
+        app_name = request.headers.get('X-App-Name')
+        app_key = request.headers.get('X-App-Key')
+        if app.config.get('REQUIRE_HEADERS', True):
+            # Require app name to match
+            if app_name is None or app_name.lower() != 'tamari':
+                return jsonify({"message": "app name is missing or incorrect"}), 401
+            # Check if the provided app_key matches the one in the configuration
+            if not secrets.compare_digest(app_key, app.config.get('APP_KEY')):
+                return jsonify({"message": "Invalid app_key"}), 401
+        # Get the identity of the user from the authorization token
+        current_user = get_jwt_identity()
+        user = User.query.filter_by(id=current_user).first_or_404()
+        if user:
+            list = Shoplist.query.filter_by(hex_id=hexid).first()
+            try:
+                listitems = Listitem.query.filter_by(list_id=list.id).all()
+            except:
+                listitems = []
+            if list is None or list.user_id != current_user:
+                return jsonify(message="The requested shopping list either cannot be found or you do not have permission to modify it."), 400
+            for item in listitems:
+                db.session.delete(item)
+            db.session.delete(list)
+            db.session.commit()
+            return jsonify(message="Success")
+        else:
+            return jsonify(message="User not found"), 400
+    else:
+        return jsonify({"message": "API is disabled"}), 503
+        
 @bp.route('/api/shopping-lists/item/<hexid>/mark', methods=['PUT'])
 @limiter.limit(Config.DEFAULT_RATE_LIMIT)
 @jwt_required()

01e55d864c95cc0027baf9ce1e4fe04f57226563
Alex Bates <alex.bates@me.com>
2025-06-17 16:57:29 -0400

    Adjustments to element scaling on Set Password and Request Reset pages

--- app/static/request-reset.js
+++ app/static/request-reset.js
@@ -2,8 +2,8 @@ function msgtoggle() {
     var emailerror = document.getElementById('erroremail');
     if (emailerror != null)
     {
-        document.getElementById("b1box").style.height="220px";
-        document.getElementById("b1box").style.marginTop="-110px";
-        document.getElementById("b1bottom").style.height="166px";
+        document.getElementById("b1box").style.height="224px";
+        document.getElementById("b1box").style.marginTop="-112px";
+        document.getElementById("b1bottom").style.height="170px";
     }
 }
--- app/static/set-password.js
+++ app/static/set-password.js
@@ -3,20 +3,20 @@ function msgtoggle() {
     var password2error = document.getElementById('errorpassword2');
     if (password1error != null && password2error != null)
     {
-        document.getElementById("b1box").style.height="291px";
-        document.getElementById("b1box").style.marginTop="-145px";
-        document.getElementById("b1bottom").style.height="237px";
+        document.getElementById("b1box").style.height="299px";
+        document.getElementById("b1box").style.marginTop="-149px";
+        document.getElementById("b1bottom").style.height="245px";
     }
     if (password1error != null && password2error == null)
     {
-        document.getElementById("b1box").style.height="275px";
-        document.getElementById("b1box").style.marginTop="-137px";
-        document.getElementById("b1bottom").style.height="221px";
+        document.getElementById("b1box").style.height="283px";
+        document.getElementById("b1box").style.marginTop="-141px";
+        document.getElementById("b1bottom").style.height="229px";
     }
     if (password1error == null && password2error != null)
     {
-        document.getElementById("b1box").style.height="275px";
-        document.getElementById("b1box").style.marginTop="-137px";
-        document.getElementById("b1bottom").style.height="221px";
+        document.getElementById("b1box").style.height="283px";
+        document.getElementById("b1box").style.marginTop="-141px";
+        document.getElementById("b1bottom").style.height="229px";
     }
 }
